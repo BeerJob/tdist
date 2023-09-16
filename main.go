c7cc72457c3865430c604ea1eb97b69ed121e606
BeerJob <root@Ubuntu22.04.3.myguest.virtualbox.org>
2023-09-15 23:44:09 -0300

    6

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 import (
 	"log"
-	//"fmt"
 	"os"
 	"strings"
 	"strconv"
@@ -9,6 +8,7 @@ import (
 	"time"
 	"context"
 
+	"github.com/streadway/ampq"
 	"google.golang.org/grpc"
 	pb "github.com/BeerJob/tdist/proto"
 )
@@ -23,18 +23,19 @@ func main(){
 	sLimit, err := strconv.Atoi(strings.SplitN(rContent[1], "\n", 2)[0])
 	amount, err := strconv.Atoi(strings.SplitN(rContent[1], "\n", 2)[1])
 	if amount==-1{
-		amount = 100000
+		amount = 1000000
 	}
 	for i:=1; i<=amount; i++{
 		created := rand.Intn(sLimit-iLimit+1)+iLimit
-		if amount==100000{
+		if amount==1000000{
 			log.Printf("Generacion %d/infinito", i)
 		}else{
 			log.Printf("Generacion %d/%d", i, amount)
 		}
-		conn, err := grpc.Dial("adr: '10.6.46.109:8080'", grpc.WithInsecure())
+		//Servidor 1
+		conn, err := grpc.Dial("10.6.46.140:8080", grpc.WithInsecure())
 		if err != nil{
-			panic("cannot connect with the server!")
+			log.Print("No se pudo conectar con Servidor1!")
 		}
 		defer conn.Close()
 		cliente := pb.NewServidorRegionalClient(conn)
@@ -42,13 +43,14 @@ func main(){
 		defer cancel()
 		r, err := cliente.CuposDisponibles(ctx, &pb.Cupo{Cupos: strconv.Itoa(created)})
 		if err != nil{
-			log.Fatal("Todo mal")
+			log.Print("No hay respuesta del Servidor1")
+		}else{
+			log.Printf("Respuesta sincrona del Servidor1: %s", r.Ok)
 		}
-		log.Printf("Respuesta de mensaje sincrono: %s", r.Ok)
-		/*
-		conn, err = grpc.Dial("adr: 'ip2:50501'", grpc.WithInsecure())
+		//Servidor2
+		conn, err = grpc.Dial("10.6.46.141:8080", grpc.WithInsecure())
 		if err != nil{
-			panic("cannot connect with the server!")
+			log.Print("No se pudo conectar con Servidor2!")
 		}
 		defer conn.Close()
 		cliente = pb.NewServidorRegionalClient(conn)
@@ -56,55 +58,227 @@ func main(){
 		defer cancel()
 		r, err = cliente.CuposDisponibles(ctx, &pb.Cupo{Cupos: strconv.Itoa(created)})
 		if err != nil{
-			log.Fatal("Todo mal")
-		}
-		log.Printf("Respuesta de mensaje sincrono: %s", r.Ok)
-		*/
-		//Codigo de la cola rabbit
-		recibido := 3
-		inscritos := 0
-		if created-recibido < 0{
-			inscritos = -(created-recibido)
-			created=0
+			log.Print("No hay respuesta del Servidor2")
 		}else{
-			created = amount-recibido
+			log.Printf("Respuesta sincrona del Servidor2: %s", r.Ok)
 		}
+		//Servidor3
 		conn, err = grpc.Dial("10.6.46.109:8080", grpc.WithInsecure())
 		if err != nil{
-			panic("cannot connect with the server!")
+			log.Print("No se pudo conectar con Servidor3!")
 		}
 		defer conn.Close()
 		cliente = pb.NewServidorRegionalClient(conn)
 		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
-		r, err = cliente.CuposRechazados(ctx, &pb.Rechazado{Rechazados: strconv.Itoa(inscritos)})
+		r, err = cliente.CuposDisponibles(ctx, &pb.Cupo{Cupos: strconv.Itoa(created)})
 		if err != nil{
-			log.Fatal("Todo mal")
+			log.Print("No hay respuesta del Servidor3")
+		}else{
+			log.Printf("Respuesta sincrona del Servidor3: %s", r.Ok)
 		}
-		log.Printf("Respuesta de mensaje sincrono: %s", r.Ok)
-		//Codigo cola rabbit
-		/*
-		recibido := 3
-		inscritos := 0
+		//Servidor4
+		conn, err = grpc.Dial("10.6.46.110:8080", grpc.WithInsecure())
+		if err != nil{
+			log.Print("No se pudo conectar con Servidor4!")
+		}
+		defer conn.Close()
+		cliente = pb.NewServidorRegionalClient(conn)
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err = cliente.CuposDisponibles(ctx, &pb.Cupo{Cupos: strconv.Itoa(created)})
+		if err != nil{
+			log.Print("No hay respuesta del Servidor4")
+		}else{
+			log.Printf("Respuesta sincrona del Servidor4: %s", r.Ok)
+		}
+		recibido := 0
+		noinscritos := 0
+		timer := time.NewTicker(10*time.Second)
+		
+		//Servidor1
+		conn, err = amqp.Dial("ampq//guest:guest@10.6.46.109:5672")
+		if err != nil{
+			log.Print("No se pudo conectar a la cola")
+		}
+		defer conn.Close()
+		channel, err := conn.Channel()
+		if err != nil{
+			log.Print("No se pudo crear canal en la cola")
+		}
+		defer channel.Close()
+		timer = time.NewTicker(10* time.Second)
+		for{
+			select{
+			case <- timer.C:
+				msg, ok, err := channel.Get("centralQueue", true)
+				if err != nil{
+					log.Print("Error al obtener mensaje de la cola")
+				}
+				if ok{
+					log.Printf("Mensaje asincrono de Servidor1 leido")
+					recibidos = msg.body
+				}
+			}
+		}
+		
 		if created-recibido < 0{
-			inscritos = -(created-recibido)
+			noinscritos = -(created-recibido)
 			created=0
 		}else{
-			created = amount-recibido
+			noinscritos=0
+			created = created-recibido
 		}
-		conn, err = grpc.Dial("adr: 'ip2:50501'", grpc.WithInsecure())
+		log.Printf("Se inscribieron %d cupos del Servidor1", recibidos-noinscritos)
+		conn, err = grpc.Dial("10.6.46.140:8080", grpc.WithInsecure())
 		if err != nil{
-			panic("cannot connect with the server!")
+			log.Print("No se puede conectar con Servidor1")
 		}
 		defer conn.Close()
 		cliente = pb.NewServidorRegionalClient(conn)
 		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
-		r, err = cliente.CuposRechazados(ctx, &pb.Rechazado{Rechazados: strconv.Itoa(inscritos)})
+		r, err = cliente.CuposRechazados(ctx, &pb.Rechazado{Rechazados: strconv.Itoa(noinscritos)})
 		if err != nil{
-			log.Fatal("Todo mal")
+			log.Print("No hay respuesta del Servidor1")
 		}
-		log.Printf("Respuesta de mensaje sincrono: %s", r.Ok)
-		*/
+
+		//Servidor2
+		conn, err = amqp.Dial("ampq//guest:guest@10.6.46.109:5672")
+		if err != nil{
+			log.Print("No se pudo conectar a la cola")
+		}
+		defer conn.Close()
+		channel, err := conn.Channel()
+		if err != nil{
+			log.Print("No se pudo crear canal en la cola")
+		}
+		defer channel.Close()
+		timer = time.NewTicker(10* time.Second)
+		for{
+			select{
+			case <- timer.C:
+				msg, ok, err := channel.Get("centralQueue", true)
+				if err != nil{
+					log.Print("Error al obtener mensaje de la cola")
+				}
+				if ok{
+					log.Printf("Mensaje asincrono de Servidor1 leido")
+					recibidos = msg.body
+				}
+			}
+		}
+		if created-recibido < 0{
+			noinscritos = -(created-recibido)
+			created=0
+		}else{
+			noinscritos=0
+			created = created-recibido
+		}
+		log.Printf("Se inscribieron %d cupos del Servidor2", recibidos-noinscritos)
+		conn, err = grpc.Dial("10.6.46.141:8080", grpc.WithInsecure())
+		if err != nil{
+			log.Print("No se puede conectar con Servidor2")
+		}
+		defer conn.Close()
+		cliente = pb.NewServidorRegionalClient(conn)
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err = cliente.CuposRechazados(ctx, &pb.Rechazado{Rechazados: strconv.Itoa(noinscritos)})
+		if err != nil{
+			log.Print("No hay respuesta del Servidor2")
+		}
+
+		//Servidor3
+		conn, err = amqp.Dial("ampq//guest:guest@10.6.46.109:5672")
+		if err != nil{
+			log.Print("No se pudo conectar a la cola")
+		}
+		defer conn.Close()
+		channel, err := conn.Channel()
+		if err != nil{
+			log.Print("No se pudo crear canal en la cola")
+		}
+		defer channel.Close()
+		timer = time.NewTicker(10* time.Second)
+		for{
+			select{
+			case <- timer.C:
+				msg, ok, err := channel.Get("centralQueue", true)
+				if err != nil{
+					log.Print("Error al obtener mensaje de la cola")
+				}
+				if ok{
+					log.Printf("Mensaje asincrono de Servidor1 leido")
+					recibidos = msg.body
+				}
+			}
+		}
+		if created-recibido < 0{
+			noinscritos = -(created-recibido)
+			created=0
+		}else{
+			noinscritos=0
+			created = created-recibido
+		}
+		log.Printf("Se inscribieron %d cupos del Servidor3", recibidos-noinscritos)
+		conn, err = grpc.Dial("10.6.46.109:8080", grpc.WithInsecure())
+		if err != nil{
+			log.Print("No se puede conectar con Servidor3")
+		}
+		defer conn.Close()
+		cliente = pb.NewServidorRegionalClient(conn)
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err = cliente.CuposRechazados(ctx, &pb.Rechazado{Rechazados: strconv.Itoa(noinscritos)})
+		if err != nil{
+			log.Print("No hay respuesta del Servidor3")
+		}
+
+		//Servidor4
+		conn, err = amqp.Dial("ampq//guest:guest@10.6.46.109:5672")
+		if err != nil{
+			log.Print("No se pudo conectar a la cola")
+		}
+		defer conn.Close()
+		channel, err := conn.Channel()
+		if err != nil{
+			log.Print("No se pudo crear canal en la cola")
+		}
+		defer channel.Close()
+		timer = time.NewTicker(10* time.Second)
+		for{
+			select{
+			case <- timer.C:
+				msg, ok, err := channel.Get("centralQueue", true)
+				if err != nil{
+					log.Print("Error al obtener mensaje de la cola")
+				}
+				if ok{
+					log.Printf("Mensaje asincrono de Servidor1 leido")
+					recibidos = msg.body
+				}
+			}
+		}
+		if created-recibido < 0{
+			noinscritos = -(created-recibido)
+			created=0
+		}else{
+			noinscritos=0
+			created = created-recibido
+		}
+		log.Printf("Se inscribieron %d cupos del Servidor4", recibidos-noinscritos)
+		conn, err = grpc.Dial("10.6.46.110:8080", grpc.WithInsecure())
+		if err != nil{
+			log.Print("No se puede conectar con Servidor4")
+		}
+		defer conn.Close()
+		cliente = pb.NewServidorRegionalClient(conn)
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err = cliente.CuposRechazados(ctx, &pb.Rechazado{Rechazados: strconv.Itoa(noinscritos)})
+		if err != nil{
+			log.Print("No hay respuesta del Servidor4")
+		}
 	}
 }
